02704d28620917ff9d2e4270fe31c6cf0c691525
Tess Roseburrough <troseburrough@dev-10.com>
2023-06-04 20:59:50 -0500

    Randomized trailer works on second page load, but it says that iframe is null-uncaught runtime error

--- client/mideo-list-react/src/components/Home.js
+++ client/mideo-list-react/src/components/Home.js
@@ -7,108 +7,81 @@ import YouTube from 'react-youtube';
     // Add learn more section
     // add sign up button
     // add log in button
-    // generate random trailer from api
 
 function Home() {
 
     const [trailer, setTrailer] = useState('');
     const [title, setTitle] = useState('');
     const [trailerId, setTrailerId] = useState('');
-    
+
     // hard-coded genreId list from API
     const genreIdList = [1,2,4,5,6,7,12,14,16,18,27,28,35,36,37,53,80,99,878,9648,10402,10749,10751,10752,10763,10764,10767];
-    console.log(genreIdList);
 
-    let genreId = 0;
+    async function getWatchableFromAPI() {
+        
+        function getRandomGenreId() {
+            const random = Math.floor(Math.random() * genreIdList.length);
+            return genreIdList[random];
+        }
+        
+        const randomGenreId = getRandomGenreId();
 
-    function getRandomGenreId() {
-        const random = Math.floor(Math.random() * genreIdList.length);
-        return genreId = genreIdList[random];
+        // Returns list of JSON watchable objects:
+        const url = `https://streaming-availability.p.rapidapi.com/v2/search/basic?country=us&services=netflix%2Cprime.buy%2Chulu.addon.hbo%2Cpeacock.free&output_language=en&show_type=all&genre=${randomGenreId}&show_original_language=en`;
+
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': '29529a668bmsh6a9f62367cbcd5ap1f0a53jsncd59c509341f',
+                'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com'
+            }
+        };
+        
+         try {
+            const response = await fetch(url, options);
+            if (response.status === 200) {
+                // data contains an array with 8 watchable objects
+                const data = await response.json();
+                //console.log(data);
+
+                function getRandomWatchableObject() {
+                    // how to access watchable obj inside data?
+                    // data.result.array.watchable?
+                    const array = data.result;
+                    // chooses between 1-8
+                    const watchable = array[Math.floor(Math.random() * array.length)];
+
+                    // console.log(watchable);
+                    return watchable;
+                }
+
+                const randomWatchableObject = getRandomWatchableObject();
+                const trailerLink = randomWatchableObject.youtubeTrailerVideoLink;
+                const watchableTitle = randomWatchableObject.title;
+                const youtubeTrailerVideoId = randomWatchableObject.youtubeTrailerVideoId;
+
+                setTrailer(trailerLink);
+                setTitle(watchableTitle);
+                setTrailerId(youtubeTrailerVideoId);
+            } else if (response.status === 400) {
+				// display some form errors
+				const errors = await response.json();
+				console.log(errors);
+			} else {
+				return Promise.reject(`Unexpected status code: ${response.status}`);
+			}
+        } catch (error) {
+            console.error(error);
+        }
     }
 
-    // is the method working?
-    console.log(getRandomGenreId());
+    useEffect(() => {
+        const fetchData = async () => {
+            await getWatchableFromAPI();
+        };
 
-    // async function getWatchableFromAPI() {
-    // // Returns list of JSON watchable objects:
-    //     const url = `https://streaming-availability.p.rapidapi.com/v2/search/basic?country=us&services=netflix%2Cprime.buy%2Chulu.addon.hbo%2Cpeacock.free&output_language=en&show_type=all&genre=${genreId}&show_original_language=en`;
-    //     const options = {
-    //         method: 'GET',
-    //         headers: {
-    //             'X-RapidAPI-Key': '29529a668bmsh6a9f62367cbcd5ap1f0a53jsncd59c509341f',
-    //             'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com'
-    //         }
-    //     };
-        
-    //     // console.log()
-
-    //     try {
-    //         const response = await fetch(url, options);
-    //         if (response.status === 200) {
-                
-    //             const data = await response.json();
-    //             // console.log(data);
-
-    //             // how to get one object? 
-    //             // dot in to a specific or random position?
-    //             // function getRandomWatchableObject() {
-    //             //     genreId = (genreIdList[(Math.floor(Math.random() * genreIdList.length))]);
-    //             // }
-
-    //             // get specific trailer
-    //             const trailerLink = data.result.youtubeTrailerVideoLink;
-    //             const watchableTitle = data.result.title;
-    //             const trailerId = data.result.youtubeTrailerVideoId;
-
-    //             setTrailer(trailerLink);
-    //             setTitle(watchableTitle);
-    //             setTrailerId(trailerId);
-    //         }
-
-    // }
-
-// async function getWatchableFromAPI() {
-//   const url =
-//     'https://streaming-availability.p.rapidapi.com/v2/get/basic?country=us&imdb_id=tt1877830&output_language=en';
-//   const options = {
-//     method: 'GET',
-//     headers: {
-//       'X-RapidAPI-Key': '29529a668bmsh6a9f62367cbcd5ap1f0a53jsncd59c509341f',
-//       'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com',
-//     },
-//   };
-
-//   try {
-//     const response = await fetch(url, options);
-//     if (response.status === 200) {
-        
-//         const data = await response.json();
-        
-//         const trailerLink = data.result.youtubeTrailerVideoLink;
-
-//         const watchableTitle = data.result.title;
-
-//         const trailerId = data.result.youtubeTrailerVideoId;
-
-//         setTrailer(trailerLink);
-//         setTitle(watchableTitle);
-//         setTrailerId(trailerId);
-//     }
-   
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
-
-// useEffect(() => {
-//   getWatchableFromAPI();
-
-// //   const intervalId = setInterval(getWatchableFromAPI, 24 * 60 * 60 * 1000); // Fetch a new trailer every 24 hours
-
-// //   return () => {
-// //     clearInterval(intervalId);
-// //   };
-// }, []);
+        fetchData();
+    }, []);
 
     return (
         <div>
@@ -132,20 +105,27 @@ function Home() {
             </div>
             <div className='container-fluid bg-transparent rounded p-5'>
                 <div className="container p-5 col-12 text-center" style={{ color: 'white'}}>
-                    <p className='lead'>Have you seen this trailer? Check it out!</p>
+                    <p className='lead'>Not sure what you want to watch? Here is a trailer to get you started!</p>
+                    {/* <div className='my-3'>
+                            <button onClick={getWatchableFromAPI} className='btn btn-light'>Trailer</button>
+                        </div> */}
                 </div>
-                <div style={{ marginLeft: 0, marginRight: 'auto', width: 'fit-content' }}>
+                <div style={{ margin: 'auto', width: 'fit-content' }}>
                     <div style={{ position: 'relative' }}>
-                        <p style={{ position: 'absolute', top: -30, left: 0, color: 'white', margin: '10px' }}>
-                        <a href={trailer} target="_blank" rel="noopener noreferrer" style={{ color: 'white' }}>
-                            {title}
-                        </a>
-                        </p>
-                        <div className="embed-responsive embed-responsive-16by9" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                            <div style={{ width: '100%', height: '100%' }}>
-                                <YouTube videoId={trailerId} title={title} />
-                            </div>
-                        </div>
+                            {trailerId && (
+                                <>
+                                    <div className="embed-responsive embed-responsive-16by9" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                                        <div style={{ width: '100%', height: '100%' }}>
+                                            <YouTube videoId={trailerId} title={title} />
+                                        </div>
+                                    </div>
+                                    <p style={{ position: 'absolute', bottom: -40, left: 0, color: 'white', margin: '10px' }}>
+                                        <a href={trailer} target="_blank" rel="noopener noreferrer" style={{ color: 'white' }}> Watch the trailer on YouTube: {' '}
+                                            {title}
+                                        </a>
+                                    </p>
+                                </>
+                            )}
                     </div>
                 </div>
             </div>
