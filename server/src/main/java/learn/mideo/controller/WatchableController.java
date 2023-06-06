42500a74e0d3d0c3dedf584301dd2a52f4ee2868
Tess Roseburrough <troseburrough@dev-10.com>
2023-06-06 10:20:10 -0500

    update progress: Invalid BSON doc for update (error)

--- server/src/main/java/learn/mideo/controller/WatchableController.java
+++ server/src/main/java/learn/mideo/controller/WatchableController.java
@@ -1,13 +1,23 @@
 package learn.mideo.controller;
 
-import learn.mideo.domain.Result;
+import com.mongodb.BasicDBObject;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.result.UpdateResult;
+import com.mongodb.internal.bulk.UpdateRequest;
 import learn.mideo.domain.WatchableService;
 import learn.mideo.model.Watchable;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin(origins = {"http://localhost:3000"})
@@ -15,22 +25,98 @@ import java.util.List;
 public class WatchableController {
 
     private final WatchableService service;
+    private final MongoTemplate mideoListDB;
 
-    public WatchableController(WatchableService service) {
+    public WatchableController(WatchableService service, MongoTemplate mideoListDB) {
         this.service = service;
+        this.mideoListDB = mideoListDB;
     }
 
-    // do we need this method? Delete if not used by deadline.
-    @GetMapping(value = "/watchableByImbdId/{imdbId}")
-    public ResponseEntity<List<Watchable>> findWatchableByImdbId(@PathVariable("imdbId") String imdbId) {
-        return ResponseEntity.ok().body(service.findWatchableByImdbId(imdbId));
+    // do we need this method when connecting to the API? Delete if not used by deadline.
+//    @GetMapping(value = "/watchableByImbdId/{imdbId}")
+//    public ResponseEntity<List<Watchable>> findWatchableByImdbId(@PathVariable("imdbId") String imdbId) {
+//        return ResponseEntity.ok().body(service.findWatchableByImdbId(imdbId));
+//    }
+
+    @GetMapping(value = "/watchableById/{id}")
+    public ResponseEntity<Watchable> findWatchableById(@PathVariable("id") String id) {
+        Optional<Watchable> optionalWatchable = service.findWatchableById(id);
+        Watchable watchable = optionalWatchable.get();
+
+        return new ResponseEntity<Watchable>(watchable, HttpStatus.OK);
     }
 
     @PostMapping
     public ResponseEntity<Object> addWatchable(@RequestBody Watchable watchable) {
         return ResponseEntity.ok().body(service.save(watchable));
     }
+
+//    @PatchMapping(value = "/updatePersonalRating/{id}")
+//    public ResponseEntity<Watchable> updateWatchablePersonalRating(
+//            @PathVariable("id") String id,
+//            @RequestParam(required = false) Integer personalRating
+//    ) {
+//        Query query = Query.query(Criteria.where("_id").is(new ObjectId(id)));
+//        Update update = new Update();
 //
-//    @PutMapping(value= "/watchable/{id}")
-//    public ResponseEntity<>
+//        if (personalRating != null) {
+//            update.set("personalRating", personalRating);
+//        }
+//
+//        UpdateResult result = mideoListDB.updateFirst(query, update, Watchable.class);
+//
+//        if (result.getModifiedCount() > 0) {
+//            Watchable updatedWatchable = service.findWatchableById(id).orElse(null);
+//            return new ResponseEntity<>(service.save(updatedWatchable), HttpStatus.OK);
+//        } else {
+//            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+//        }
+//    }
+//@PatchMapping(value = "/updatePersonalRating/{id}")
+//public ResponseEntity<Watchable> updateWatchablePersonalRating(
+//        @PathVariable("id") String id,
+//        @RequestParam(required = false) Integer personalRating
+//) {
+//    BasicDBObject query = new BasicDBObject("_id", new ObjectId(id));
+//    BasicDBObject update = new BasicDBObject();
+//
+//    if (personalRating != null) {
+//        update.put("$set", new BasicDBObject("personalRating", personalRating));
+//    }
+//
+//    MongoDatabase database = mideoListDB.getDb();
+//    MongoCollection<Document> collection = database.getCollection("watchable");
+//    UpdateResult result = collection.updateOne(query, update);
+//
+//    if (result.getModifiedCount() > 0) {
+//        Watchable updatedWatchable = service.findWatchableById(id).orElse(null);
+//        return new ResponseEntity<>(service.save(updatedWatchable), HttpStatus.OK);
+//    } else {
+//        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+//    }
+//}
+
+    @PatchMapping(value = "/updatePersonalRating/{id}")
+    public ResponseEntity<Watchable> updateWatchablePersonalRating(
+            @PathVariable("id") String id,
+            @RequestParam(required = false) Integer personalRating
+    ) {
+        Document query = new Document("_id", new ObjectId(id));
+        Document update = new Document();
+
+        if (personalRating != null) {
+            update.put("$set", new Document("personalRating", personalRating));
+        }
+
+        MongoDatabase database = mideoListDB.getDb();
+        MongoCollection<Document> collection = database.getCollection("watchable");
+        UpdateResult result = collection.updateOne(query, update);
+
+        if (result.getModifiedCount() > 0) {
+            Watchable updatedWatchable = service.findWatchableById(id).orElse(null);
+            return new ResponseEntity<>(service.save(updatedWatchable), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
--- server/src/main/java/learn/mideo/domain/WatchableService.java
+++ server/src/main/java/learn/mideo/domain/WatchableService.java
@@ -1,23 +1,31 @@
 package learn.mideo.domain;
 
+import com.mongodb.client.MongoDatabase;
 import learn.mideo.data.WatchableRepository;
 import learn.mideo.model.Watchable;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class WatchableService {
+
     private final WatchableRepository repository;
 
     public WatchableService(WatchableRepository repository) {
         this.repository = repository;
     }
-    public List<Watchable> findWatchableByImdbId(String imdbId) {
-        return repository.findWatchableById(imdbId);
-    }
+//    public List<Watchable> findWatchableByImdbId(String imdbId) {
+//        return repository.findWatchableByImdbId(imdbId);
+//    }
+
     public Watchable save(Watchable watchable) {
         return repository.save(watchable);
     }
 
+    public Optional<Watchable> findWatchableById(String id) {
+        return repository.findById(id);
+    }
 }
