df7ca499eb398136869ae631d3a76a9ab194e162
Anthony Rocco-Frank <106894159+AnthonyRoccoFrank@users.noreply.github.com>
2023-06-07 22:47:22 -0500

    Filter lists by Title, Score, or Year

--- client/mideo-list-react/src/components/CompletedMoviesTable.js
+++ client/mideo-list-react/src/components/CompletedMoviesTable.js
@@ -8,6 +8,7 @@ function CompletedMoviesTable() {
   const { user } = useContext(AuthContext);
   const [watchables, setWatchables] = useState([]);
   const [watchlist, setWatchlist] = useState([]);
+  const [sortBy, setSortBy] = useState("");
   const navigate = useNavigate();
   const padding = { padding: "15px" };
 
@@ -21,6 +22,41 @@ function CompletedMoviesTable() {
   const allWatchables = watchables.watchables;
   const watchlistId = watchables.id;
 
+  const handleSort = (event) => {
+    const selectedSortBy = event.target.value;
+    setSortBy(selectedSortBy);
+
+    // Sort the watchables based on the selected option
+    let sortedWatchables = [...allWatchables];
+    switch (selectedSortBy) {
+      case "title (a-z)":
+        sortedWatchables.sort((a, b) =>
+          a.title.localeCompare(b.title, undefined, { sensitivity: "base" })
+        );
+        break;
+      case "title (z-a)":
+        sortedWatchables.sort((a, b) =>
+          b.title.localeCompare(a.title, undefined, { sensitivity: "base" })
+        );
+        break;
+      case "year (new)":
+        sortedWatchables.sort((a, b) => b.year - a.year);
+        break;
+      case "year (old)":
+        sortedWatchables.sort((a, b) => a.year - b.year);
+        break;
+      case "score (low)":
+        sortedWatchables.sort((a, b) => a.personalRating - b.personalRating);
+        break;
+      case "score (high)":
+        sortedWatchables.sort((a, b) => b.personalRating - a.personalRating);
+        break;
+      default:
+        break;
+    }
+    setWatchables({ ...watchables, watchables: sortedWatchables });
+  };
+
   return (
     <div className="p-5">
       {allWatchables && (
@@ -33,6 +69,27 @@ function CompletedMoviesTable() {
               Completed Movies List
             </h2>
           </div>
+
+          <div className="mb-3 white-text">
+            <label htmlFor="sortSelect" className="form-label">
+              Sort By:
+            </label>
+            <select
+              id="sortSelect"
+              className="form-select"
+              value={sortBy}
+              onChange={handleSort}
+            >
+              <option value="">None</option>
+              <option value="title (a-z)">Title (A-Z)</option>
+              <option value="title (z-a)">Title (Z-A)</option>
+              <option value="year (new)">Year (New)</option>
+              <option value="year (old)">Year (Old)</option>
+              <option value="score (low)">Score (Low)</option>
+              <option value="score (high)">Score (High)</option>
+            </select>
+          </div>
+
           <table className="table table-striped table-hover table-dark">
             <thead>
               <tr>
